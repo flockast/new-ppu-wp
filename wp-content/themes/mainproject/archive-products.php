3e776ca923518216a1c9976d90b536cfc69a15b8
Konstantin Vlasov <webvlassov@gmail.com>
2015-03-16 19:10:27 +0300

    Изменение логотипа и цвета меню

---
+++ wp-content/themes/mainproject/archive-products.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>
+
+<div class="mainContent">
+	<input type="hidden" name="data-file" value="archive-products.php">
+	<h2 class="topTitle">Компоненты, оборудование и комплектующие</h2>
+	
+	<div class="pageProducts">
+		<div class="oneProduct">
+		  <div class="productImage">
+		    <a href="#">
+		      <img src="img/content/product.jpg" alt="">
+		    </a>
+		  </div>
+		  <div class="productTitle">
+		    <a href="#">Бочка красная с синим</a>
+		  </div>
+		</div>
+	</div>
+</div>
+<div class="mainSidebar">
+	<?php get_sidebar(); ?>
+</div>
+<?php get_footer(); ?>
